f018093a11da528aecb90db5f441d4bef364666d
GCassella <gwc22@bath.ac.uk>
2020-07-27 12:34:15 +0100

    Pythonized the kernel import scripts

--- mcramp/geom/__init__.py
+++ mcramp/geom/__init__.py
@@ -1,6 +1,17 @@
-from .plane import GPlane
-from .banana import GBanana
-from .sphere import GSphere
-from .arm import GArm
-from .box import GBox
-from .cylinder_ext import GCylinderExt
+from inspect import isclass
+from pkgutil import iter_modules
+from pathlib import Path
+from importlib import import_module
+
+# iterate through the modules in the current package
+package_dir = Path(__file__).resolve().parent
+for (_, module_name, _) in iter_modules([package_dir]):
+
+    # import the module and iterate through its attributes
+    module = import_module(f"{__name__}.{module_name}")
+    for attribute_name in dir(module):
+        attribute = getattr(module, attribute_name)
+
+        if isclass(attribute):            
+            # Add the class to this package's variables
+            globals()[attribute_name] = attribute
