9265d756d9581f6c0d773ab6ed6eaa37e28b6dfe
GCassella <gwc22@bath.ac.uk>
2020-07-27 12:34:49 +0100

    Created beamstop, and ideal polariser and analyser kernels

---
+++ mcramp/scat/analyser.py
@@ -0,0 +1,42 @@
+from .sprim import SPrim
+
+import pyopencl as cl
+import pyopencl.array as clarr
+import numpy as np
+import os
+
+class SAnalyser(SPrim):
+    """
+    
+
+    Parameters
+    ----------
+    
+
+    Methods
+    -------
+    Data
+        None
+    Plot
+        None
+    Save
+        None
+
+    """
+
+    def __init__(self, polarisation = [0, 0, 0], idx=0, ctx=None, filename=None, **kwargs):
+        mf = cl.mem_flags
+        self.idx = np.uint32(idx)
+
+        self.polarisation = np.array((polarisation[0], polarisation[1], polarisation[2], 0.0), dtype=clarr.vec.float3)
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'analyser.cl'), mode='r') as f:
+            self.prg = cl.Program(ctx, f.read()).build(options=r'-I "{}/include"'.format(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+    def scatter_prg(self, queue, N, neutron_buf, intersection_buf, iidx_buf):
+        self.prg.analyser(queue, (N, ),
+                          None,
+                          neutron_buf,
+                          intersection_buf,
+                          iidx_buf,
+                          self.idx,
+                          self.polarisation)
