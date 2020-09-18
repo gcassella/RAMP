7ca1fd501a33848decaa1328af4426ebc0beeb16
GCassella <gwc22@bath.ac.uk>
2020-09-18 10:36:05 +0100

    Fixed keyerror for missing environment variable

--- mcramp/scat/guide.py
+++ mcramp/scat/guide.py
@@ -57,7 +57,8 @@ class SGuide(SPrim):
                  R0=0, Qc=0, alpha=0, m=1, W=0, idx=0, ctx=0, max_bounces=50,
                  **kwargs):
 
-        if "CONFIG_USE_DOUBLE=1" in os.environ["PYOPENCL_BUILD_OPTIONS"]:
+        if (("PYOPENCL_BUILD_OPTIONS" in os.environ)
+        and ("CONFIG_USE_DOUBLE=1" in os.environ["PYOPENCL_BUILD_OPTIONS"])):
             self.w1     = np.float64(w1)
             self.h1     = np.float64(h1)
             self.w2     = np.float64(w2)
